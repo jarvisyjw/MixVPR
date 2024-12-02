c4f21f84d413c21733389df48f1ab26a630ad533
jyubt <jyubt@connect.ust.hk>
2024-12-02 11:08:08 +0800

    torch hub support

--- hubconf.py
+++ hubconf.py
@@ -5,12 +5,57 @@ def get_trained_model(pretrained=True, **kwargs):
     """
     Loads the model. If `pretrained` is True, it loads pre-trained weights.
     """
-    model = VPRModel(**kwargs)  # Replace with your model initialization
+    
+    model = VPRModel(
+        #---- Encoder
+        backbone_arch='resnet50',
+        pretrained=True,
+        layers_to_freeze=2,
+        layers_to_crop=[4], # 4 crops the last resnet layer, 3 crops the 3rd, ...etc
+        
+        #---- Aggregator
+        # agg_arch='CosPlace',
+        # agg_config={'in_dim': 2048,
+        #             'out_dim': 2048},
+        # agg_arch='GeM',
+        # agg_config={'p': 3},
+        
+        # agg_arch='ConvAP',
+        # agg_config={'in_channels': 2048,
+        #             'out_channels': 2048},
+
+        agg_arch='MixVPR',
+        agg_config={'in_channels' : 1024,
+                'in_h' : 20,
+                'in_w' : 20,
+                'out_channels' : 1024,
+                'mix_depth' : 4,
+                'mlp_ratio' : 1,
+                'out_rows' : 4}, # the output dim will be (out_rows * out_channels)
+        
+        #---- Train hyperparameters
+        lr=0.05, # 0.0002 for adam, 0.05 or sgd (needs to change according to batch size)
+        optimizer='sgd', # sgd, adamw
+        weight_decay=0.001, # 0.001 for sgd and 0 for adam,
+        momentum=0.9,
+        warmpup_steps=650,
+        milestones=[5, 10, 15, 25, 45],
+        lr_mult=0.3,
+
+        #----- Loss functions
+        # example: ContrastiveLoss, TripletMarginLoss, MultiSimilarityLoss,
+        # FastAPLoss, CircleLoss, SupConLoss,
+        loss_name='MultiSimilarityLoss',
+        miner_name='MultiSimilarityMiner', # example: TripletMarginMiner, MultiSimilarityMiner, PairMarginMiner
+        miner_margin=0.1,
+        faiss_gpu=False
+    )
+    
     if pretrained:
         state_dict = torch.hub.load_state_dict_from_url(
             'https://raw.githubusercontent.com/jarvisyjw/MixVPR/blob/main/model.ckpt',  # Update with your weights URL
             map_location=torch.device('cpu')  # Change if GPU loading is needed
         )
-        print(state_dict)
+        
         model.load_state_dict(state_dict)
     return model
